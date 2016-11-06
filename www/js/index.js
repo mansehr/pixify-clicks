60e0a2a32973c4a5d008d4be630b8a614a5b9656
Andreas Sehr <andreas@mansehr.se>
2016-11-06 01:31:53 +0100

    Added the 40x30 canvas with 256 colors, pre server updates

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,26 @@
 var BACKEND_URL = 'https://pixify-clicks.herokuapp.com/';
-
+var NUM_COLORS = 256;
+var MIN_UPDATE = 33;
 var app = {
+    lstUpd: 0,
+    x : -1,
+    y : -1,
     // Application Constructor
     initialize: function() {
         this.connectSocket();
+        //this.updateColor();
         countCalc.calcClickSpeed();
+        /*console.log = function() {
+          debug.apply(this, arguments);
+        };*/
+        /*setInterval(function () {
+          var x = rand(0, painter.width-1);
+          var y = rand(0, painter.height-1);
+          var lstUpd = this.clicks;
+          var idx = rand(0, 255);
+          this.newPosition(x, y, idx, lstUpd);
+        }.bind(this), 10);*/
+        this.newPosition(-1,-1,0,0);
     },
     connectSocket: function() {
       var socket = this.socket = io(BACKEND_URL);
@@ -15,18 +31,42 @@ var app = {
 
       socket.on('c', function() {
         ++this.clicks;
-        this.updateText();
+        this.updateScreen();
+      }.bind(this));
+
+      socket.on('p', function(x, y, c) {
+        painter.paintCell(x, y, c);
+      }.bind(this));
+
+      socket.on('t', function(x, y, t) {
+        this.newPosition(x, y, t);
       }.bind(this));
 
       socket.on('k', function(newClicks) {
         this.clicks = newClicks;
-        this.updateText();
+        this.updateScreen();
       }.bind(this));
 
       socket.on('u', function(newClickers) {
         this.clickers = newClickers;
-        this.updateText();
+        this.updateScreen();
       }.bind(this));
+    },
+
+    newPosition: function(newX, newY, lastUpdate) {
+      // Clear border before update
+      this.refreshActiveCell(false);
+
+      this.x = newX;
+      this.y = newY;
+      this.lstUpd = lastUpdate;
+      this.updateColor();
+      console.log('new pos:', this.x, this.y);
+    },
+    refreshActiveCell: function(showBorder) {
+      var idx = this.clicks - this.lstUpd;
+      painter.paintBorder(this.x,this.y, showBorder);
+      painter.paintCell(this.x, this.y, idx);
     },
 
     sendClick: function() {
@@ -46,27 +86,42 @@ var app = {
       this.sendClick();
       return false;
     },
+    updateColor: function() {
+        var idx = this.clicks - this.lstUpd;
+        var color = COLORS[idx%NUM_COLORS];
+        this.getElement('color_holder').style.background = '#'+color;
+        if(color === "ffffff") {
+          this.getElement('color_holder').style.color = '#000000';
+        } else {
+          this.getElement('color_holder').style.color = '#ffffff';
+        }
+        painter.paintCell(app.x, app.y, idx);
+    },
 
     _lastUpdate : Date.now(),
     _paintTimer : undefined,
-    updateText: function () {
+    updateScreen: function () {
       // Only do 30 updates each second
-      if((Date.now() - this._lastUpdate) > 33 ) {
+      if((Date.now() - this._lastUpdate) > MIN_UPDATE ) {
         this.setText('clickersElement', this.clickers);
         this.setText('clicksElement', this.clicks);
         this.setText('speedElement', this.speed);
+        this.updateColor();
         this._lastUpdate = Date.now();
       } else {
         clearTimeout(this._paintTimer);
-        this._paintTimer = setTimeout(this.updateText.bind(this), 33);
+        this._paintTimer = setTimeout(this.updateScreen.bind(this), MIN_UPDATE);
       }
     },
     _elements : {},
     setText: function(id, value) {
+      this.getElement(id).innerText = value;
+    },
+    getElement: function(id) {
       if(this._elements[id] === undefined) {
         this._elements[id] = document.getElementById(id);
       }
-      this._elements[id].innerText = value;
+      return this._elements[id];
     }
 };
 var countCalc = {
@@ -86,11 +141,21 @@ var countCalc = {
     lastClickCount = app.clicks;
     setTimeout(this.calcClickSpeed.bind(this), 1000 / this._buckets);
     if(hasChanged) {
-      app.updateText();
+      app.updateScreen();
     }
   }
 };
-function debug(msg) {
+function debug() {
+  if(arguments.length === 0) {
+    return;
+  }
   var el = document.getElementById('debug_log');
-  el.innerHTML = el.innerHTML + '\n' + msg;
+  for (i = 0; i < arguments.length; i++) {
+    el.innerHTML = el.innerHTML + (i > 0 ? ',' : '') + JSON.stringify(arguments[i]);
+  }
+  el.innerHTML = el.innerHTML + '\n';
+}
+
+function rand(min, max) {
+  return Math.round(min + (max * Math.random()));
 }
---
+++ www/js/painter.js
@@ -0,0 +1,45 @@
+var painter = {
+  context: undefined,
+  size : 8, grid : 1,
+  width: 40,
+  height: 30,
+  initialize: function(id) {
+    var canvas = document.getElementById(id);
+    this.context = canvas.getContext("2d");
+    //this.paintPalette();
+
+  },
+  paintPalette: function() {
+    var x = 0, y = 0, cid = 0;
+    for(y = 0; y < this.height; ++y) {
+      for(x = 0; x < this.width; ++x) {
+        this.paintCell(x, y, cid);
+        cid++;
+      }
+    }
+  },
+  paintBorder: function(x, y, on) {
+    var ctx = this.context, size = this.size;
+    var cx = (x * size),
+        cy = (y * size),
+        s = size;
+    if(on) {
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(cx + 1, cy + 1, s - 1, s - 1);
+    } else {
+      ctx.clearRect(cx, cy, s + 1, s + 1);
+    }
+  },
+  paintCell: function(x, y, colorId) {
+    var ctx = this.context, size = this.size, grid = this.grid;
+    ctx.fillStyle = toRGBString(COLORS[colorId%256]);
+    ctx.fillRect((x * size) + 1, (y * size) + 1, size - grid, size - grid);
+  }
+};
+
+function toRGBString(c) {return 'rgb('+hexToR(c)+','+hexToG(c)+','+hexToB(c)+')';}
+function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16);}
+function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16);}
+function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16);}
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h;}
